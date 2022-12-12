d4cce6b87d66ea0cb94c5a69f112543d828dbea8
Doan Thuan <doan.quang.thuan1234@gmail.com>
2022-12-12 23:15:06 +0700

    Commit code feature/b

---
+++ PersonService.java
@@ -0,0 +1,26 @@
+package com.trungtamjava.master.hellospring;
+
+import com.trungtamjava.master.hellospring.controller.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    PersonRepo personRepo;
+
+    public List<Person> getAll() {
+        return personRepo.findAll();
+    }
+    public Optional<Person> getById(Integer id) {
+        return personRepo.findById(id);
+    }
+
+    public void logout() {
+        System.out.println("User have logout.");
+    }
+}
